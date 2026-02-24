5abb24fab996502a4a10059eb4a220c59dbf7014
valmg11 <tictactina05@gmail.com>
2026-02-24 14:51:30 -0500

    edit components

--- exercise-app/src/App.js
+++ exercise-app/src/App.js
@@ -1,26 +1,32 @@
 // import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from 'react'
+// import {useState, useEffect} from 'react'
 import RepetitionExercise from "./components/RepetitionExercise"
+import DurationExercise from "./components/DurationExercise"
+
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  useEffect(()=> {
-    // console.log("button clicked "+ count+ " times");
-  }, []);
+  // useEffect(()=> {
+  //   // console.log("button clicked "+ count+ " times");
+  // }, []);
   
-  // const 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Increase</button>
-        {/* change below? */}
-        <button onClick={() => setCount(count*0)}>Reset</button>
-      </header>
-    </div>
-  );
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>Repetition Exercise</p>
+    //     <span>{count}</span>
+    //     <button onClick={() => setCount(count + 1)}>Increase</button>
+    //     {/* change below setCount? */}
+    //     <button onClick={() => setCount(count*0)}>Reset</button>
+    //   </header>
+    // </div>
+    <RepetitionExercise></RepetitionExercise>
+
+    
+    // <DurationExercise></DurationExercise>
+  );  
 }
 
 export default App;
--- exercise-app/src/components/RepetitionExercise/index.js
+++ exercise-app/src/components/RepetitionExercise/index.js
@@ -1,1 +1,24 @@
 //Repetition
+import {useState, useEffect} from 'react'
+
+
+function RepetitionExercise() {
+    const [count, setCount] = useState(0);
+    useEffect(()=> {
+        // console.log("button clicked "+ count+ " times");
+    }, [count]);
+    return (
+    <div className="App">
+        <header className="App-header">
+        <p>Repetition Exercise</p>
+        <span>{count}</span>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+        {/* change below setCount? */}
+        <button onClick={() => setCount(count*0)}>Reset</button>
+        </header>
+    </div>
+
+    );
+}
+
+export default RepetitionExercise
